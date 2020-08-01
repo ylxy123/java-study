8cd179099dad51ba50e466939d0bcf201bace668
ylxy123 <1205695200@qq.com>
2020-08-01 21:13:12 +0800

    %date:~0,4%年%date:~5,2%月%date:~8,2%日

--- 练习/src/file/TestFile.java
+++ 练习/src/file/TestFile.java
@@ -1,19 +1,43 @@
 package file;
-
+   
 import java.io.File;
-
+   
 public class TestFile {
+      
+    static long minSize = Integer.MAX_VALUE;
+    static long maxSize = 0;
+    static File minFile = null;
+    static File maxFile = null;
+      
+    //使用递归来遍历一个文件夹的子文件
+    public static void listFiles(File file){
+        if(file.isFile()){
+            if(file.length()>maxSize){
+                maxSize = file.length();
+                maxFile = file;
+            }
+            if(file.length()!=0 && file.length()<minSize){
+                minSize = file.length();
+                minFile = file;
+            }
+            return;
+        }
+          
+        if(file.isDirectory()){
+            File[] fs = file.listFiles();
+            if(null!=fs)
+            for (File f : fs) {
+                listFiles(f);
+            }
+        }
+    }
+   
     public static void main(String[] args) {
-        // 絕對路徑
-        File f1 = new File("/home");
-        System.out.println("f1的絕對路徑 : " + f1.getAbsolutePath());
-        // 相對路徑, 相對於工作目錄
-        File f2 = new File("11");
-        System.out.println("f2的絕對路徑：" + f2.getAbsolutePath());
-
-        // 以f1爲父目錄
-        File f3 = new File(f1, "11");
-        System.out.println("f3的絕對路徑：" + f3.getAbsoluteFile());
-
+        File f = new File("/home/ylxy/Desktop");
+        listFiles(f);
+        System.out.printf("最大的文件是%s，其大小是%,d字节%n",maxFile.getAbsoluteFile(),maxFile.length());
+        System.out.printf("最小的文件是%s，其大小是%,d字节%n",minFile.getAbsoluteFile(),minFile.length());
+   
     }
 }
+
